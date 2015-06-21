baa2c0901aacbc44666acdf4e044c3b10f875b8d
Michael Burns <michael.burns@rackspace.com>
2015-06-21 03:20:16 -0700

    refactor password.rb for simplicity

--- recipes/passwords.rb
+++ recipes/passwords.rb
@@ -1,15 +1,16 @@
+# load encfs passwords from encrypted data_bags
 unless node.run_state['encfs']
-  if node['encfs']['passwords']['data_bag']['enabled']
-    begin
-      unless node['encfs']['passwords']['data_bag']['encrypted']
-        bag = data_bag_item(
-          node['encfs']['passwords']['data_bag']['name'],
-          node['encfs']['data_bag']['item']
-        )
-      end
-      node.run_state['encfs'] = Mash.new(bag.to_hash)
-    rescue 404
-      Chef::Log.error('Failed to locate encfs password data bag!')
+  next unless node['encfs']['passwords']['data_bag']['enabled']
+
+  begin
+    unless node['encfs']['passwords']['data_bag']['encrypted']
+      bag = data_bag_item(
+        node['encfs']['passwords']['data_bag']['name'],
+        node['encfs']['data_bag']['item']
+      )
     end
+    node.run_state['encfs'] = Mash.new(bag.to_hash)
+  rescue 404
+    Chef::Log.error('Failed to locate encfs password data bag!')
   end
 end
